f986afcc36c07754b0aaaf37da29101a452fde1d
trepidacious <trepidacious@gmail.com>
2018-12-28 13:39:25 +0000

    Add demo of missing data
    Attempting to use an Id not in the data displays the default error, listing missing Ids. Putting a value into data for that Id triggers a render, showing the data.

--- scalajs-electron-react-app/js/src/main/scala/org/rebeam/demo/DataContextDemo.scala
+++ scalajs-electron-react-app/js/src/main/scala/org/rebeam/demo/DataContextDemo.scala
@@ -14,7 +14,8 @@ object DataContextDemo {
   val id0 = Id[String](Guid.raw(0,0,0))
   val id1 = Id[String](Guid.raw(0,0,1))
   val id2 = Id[String](Guid.raw(0,0,2))
-  val ids = List(id0, id1, id2)
+  val id3 = Id[String](Guid.raw(0,0,3)) //Deliberately not in MapDataSource
+  val ids = List(id0, id1, id2, id3)
 
   val initial = MapDataSource.empty
     .put[String](id0, "Zero", RevId(Guid.raw(0,0,100)))
@@ -30,6 +31,13 @@ object DataContextDemo {
       id <- ids.lift.apply(i)
       r <- m.getWithRev(id)
     } yield m.modify[String](id, (s: String) => s + "!", incrementRev(r._2))
+    mod.getOrElse(m)
+  }
+
+  def create(i: Int, m: MapDataSource): MapDataSource = {
+    val mod = for {
+      id <- ids.lift.apply(i)
+    } yield m.put(id, s"New value $i", RevId(Guid.raw(0,0,100+i)))
     mod.getOrElse(m)
   }
 
@@ -53,6 +61,7 @@ object DataContextDemo {
           itemDisplay(id0),
           itemDisplay(id1),
           itemDisplay(id2),
+          itemDisplay(id3),
           <.button(
             ^.onClick --> (scope.modState(exclaim(0, _)) >> Callback.log("Exclaim 0")),
             "Exclaim 0!"
@@ -64,6 +73,10 @@ object DataContextDemo {
           <.button(
             ^.onClick --> (scope.modState(exclaim(2, _)) >> Callback.log("Exclaim 2")),
             "Exclaim 2!"
+          ),
+          <.button(
+            ^.onClick --> (scope.modState(create(3, _)) >> Callback.log("Exclaim 2")),
+            "Create 3"
           )
         )
       )
