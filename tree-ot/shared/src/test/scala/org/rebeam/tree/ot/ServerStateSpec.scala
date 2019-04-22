0aa6a3a1875b11f5e8bcde6c996d786ca08346e0
trepidacious <trepidacious@gmail.com>
2019-04-22 13:47:51 +0100

    Fix and add comments to ServerStateSpec test

--- tree-ot/shared/src/test/scala/org/rebeam/tree/ot/ServerStateSpec.scala
+++ tree-ot/shared/src/test/scala/org/rebeam/tree/ot/ServerStateSpec.scala
@@ -1,5 +1,6 @@
 package org.rebeam.tree.ot
 
+import org.rebeam.tree.ot.Atom._
 import org.scalatest._
 import org.scalatest.prop.Checkers
 
@@ -13,25 +14,34 @@ class ServerStateSpec extends WordSpec with Matchers with Checkers {
       val s0 = ServerState.empty[Char]
 
       // Operation 0, on revision 0, producing revision 1
-      val op0 = Operation.fromAtoms(List(Atom.Insert("Hello World".toList)))
+      val op0 = Operation.fromAtoms(List(Insert("Hello World".toList)))
       val (s1, op0b) = s0.updated(OpRev(op0, Rev(0)))
+
+      //Operation has been applied to list, on revision 0, with no
+      //transformation required to operation
       assert(s1.list == "Hello World".toList)
       assert(op0b == OpRev(op0, Rev(0)))
 
       // Operation 1, will be applied on revision 1, producing revision 2
       val op1 = Operation.fromAtoms(List(Atom.Insert("!".toList)))
-      // Note we apply this at revions 0 - it will be transformed to match
+      // Note we apply this at revision 0 - it will be transformed to match
       // revision 1 (the current revision), and applied to revision 1
       val (s2, op1b) = s1.updated(OpRev(op1, Rev(0)))
-      assert(s2.list == "!Hello World".toList)
+
+      //Note that both operation 0 and operation 1 attempt to insert at the
+      //start of the list, at revision 0. However operation 0 gets there first,
+      //and so has higher priority - the inserted contents come first in the
+      //resulting list. This puts the '!' on the end of the result.
+      assert(s2.list == "Hello World!".toList)
+
       // Operation is updated to retain the new "Hello World" contents that
       // were added by operation 0
       assert(
         op1b == OpRev(
           Operation.fromAtoms[Char](
             List(
-              Atom.Insert("!".toList),
-              Atom.Retain(11)
+              Atom.Retain(11),
+              Atom.Insert("!".toList)
             )
           ),
           Rev(1)
